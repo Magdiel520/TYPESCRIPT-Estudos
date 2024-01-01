f5696eefe3d2769d6d4765db2134ba52e7cc853e
Magdiel520 <rotta.aero@gmail.com>
2024-01-01 20:21:46 -0300

    feat: 002 string number boolean

---
+++ 001 - Inicio/002 - Tipos/001 - String - Number - Boolean/index.ts
@@ -0,0 +1,14 @@
+let pedido: string = "Pizza";
+pedido = "Bla";
+
+let numero: number = 123;
+numero = 321;
+
+let deuBom: boolean = false;
+deuBom = true;
+
+const fun = (value: string): string =>{
+    return "Bla";
+};
+
+console.log(fun("Passarinho Azul"));
